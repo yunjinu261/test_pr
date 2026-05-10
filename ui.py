ff39fc8c9e5c8034bff9c8632c74033642d760b6
yunjinu260 <yjw9976@naver.com>
2026-05-10 23:16:22 +0900

    Set font for lbl1 in ui.py

--- ui.py
+++ ui.py
@@ -3,7 +3,7 @@ from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QVBoxLayout, QM
                              QLineEdit, QComboBox, QLabel)
 
 
-from PyQt5.QtGui import QIcon
+from PyQt5.QtGui import QIcon, QFont
 from PyQt5 import QtCore
 
 class View(QWidget):
@@ -34,6 +34,7 @@ class View(QWidget):
         self.te1.setReadOnly(True)
         
         self.lbl1 = QLabel('v2.3.0', self)
+        self.lbl1 .setFont(QFont('Concsolas', 10))
         self.btn1=QPushButton('Calc', self)
         self.btn2=QPushButton('Clear', self)
         
